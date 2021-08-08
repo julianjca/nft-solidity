4a27386149c26bcaef13cf12b2110cd47d37a114
Julian Anderson <juliancanderson@gmail.com>
2021-08-08 19:25:20 +0700

    compiled contracts

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.3;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyToken is ERC721 {
+  constructor(string memory name, string memory symbol)
+        ERC721(name, symbol) public
+  {}
+}
